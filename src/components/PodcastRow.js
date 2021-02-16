1d3d760328f35178d3dabf4e8fe9d04aeaab763a
onias17 <oniasnephiisrael@gmail.com>
2021-02-16 14:41:03 -0800

    episodes and podcasts

--- src/components/PodcastRow.js
+++ src/components/PodcastRow.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const PodcastRow = () => {
+const PodcastRow = ({name, image, categories}) => {
   return (
     <li>
       <a href="#" className="d-flex align-items-center  p-2">
-        <img src="images/person_1.jpg" alt="Image" className="img-fluid mr-2" />
+        <img src={image} alt="Image" className="img-fluid mr-2" />
         <div className="podcaster">
-          <span className="d-block">Claire Stanford</span>
-          <span className="small">32,420 podcasts</span>
+          <span className="d-block">{name}</span>
+          <span className="small">{categories.join(', ')}</span>
         </div>
       </a>
     </li>
--- src/index.js
+++ src/index.js
@@ -5,6 +5,20 @@ import Header from './components/Header'
 import PodcastRow from './components/PodcastRow'
 
 const App = () => {
+  const podcasts = [
+    {id: 0, name: 'podcast 1', image: '/images/person_1.jpg', categories: ['sports', 'entertainment']},
+    {id: 1, name: 'podcast 2', image: '/images/person_2.jpg', categories: ['news', 'politics']},
+    {id: 2, name: 'podcast 3', image: '/images/person_3.jpg', categories: ['business', 'economics']},
+  ]
+  
+  const episodes = [
+    {id: 0, title: 'track 1', image: '/images/img_1.jpg', trackUrl: 'http://hwcdn.libsyn.com/p/e/2/d/e2d49676d65218ec/p1541a.mp3?c_id=84308228&cs_id=84308228&expiration=1601254668&hwt=ccab3206052417d0e901722ab00c9c88'},
+    {id: 1, title: 'track 2', image: '/images/img_2.jpg', trackUrl: 'http://hwcdn.libsyn.com/p/e/2/d/e2d49676d65218ec/p1541a.mp3?c_id=84308228&cs_id=84308228&expiration=1601254668&hwt=ccab3206052417d0e901722ab00c9c88'},
+    {id: 2, title: 'track 3', image: '/images/img_3.jpg', trackUrl: 'http://hwcdn.libsyn.com/p/e/2/d/e2d49676d65218ec/p1541a.mp3?c_id=84308228&cs_id=84308228&expiration=1601254668&hwt=ccab3206052417d0e901722ab00c9c88'},
+    {id: 3, title: 'track 4', image: '/images/img_4.jpg', trackUrl: 'http://hwcdn.libsyn.com/p/e/2/d/e2d49676d65218ec/p1541a.mp3?c_id=84308228&cs_id=84308228&expiration=1601254668&hwt=ccab3206052417d0e901722ab00c9c88'},
+    {id: 4, title: 'track 5', image: '/images/img_5.jpg', trackUrl: 'http://hwcdn.libsyn.com/p/e/2/d/e2d49676d65218ec/p1541a.mp3?c_id=84308228&cs_id=84308228&expiration=1601254668&hwt=ccab3206052417d0e901722ab00c9c88'},
+  ]
+
   return (
     <div className="site-wrap">
       <Header />
@@ -19,18 +33,12 @@ const App = () => {
                   <button className="btn btn-info p-1 ml-2" style={{ height: 32 }}>GO!</button>
                 </div>
                 <ul className="list-unstyled">
-                  <PodcastRow />
-                  <PodcastRow />
-                  <PodcastRow />
-                  <PodcastRow />
+                  {podcasts.map(podcast => <PodcastRow key={podcast.id} {...podcast}/>)}
                 </ul>
               </div>
             </div>
             <div className="col-lg-9">
-              <Episode /> 
-              <Episode /> 
-              <Episode /> 
-              <Episode /> 
+              {episodes.map(episode => <Episode key={episode.id} {...episode}/>)}
             </div>
           </div>
         </div>
